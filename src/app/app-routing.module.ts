849ff8c9be00dbb078acfd61fa7c9e308c572bc6
Vinicius Santos <viniciushacrinha1@gmail.com>
2021-08-28 06:39:59 -0300

    Criando as rotas e Criando mais componentes, Perfil e DashBoard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DesenvolvedoresComponent } from './desenvolvedores/desenvolvedores.component';
+import { ProjetosComponent } from './projetos/projetos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'desenvolvedores', component: DesenvolvedoresComponent},
+  {path: 'projetos', component: ProjetosComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProjetosComponent } from './projetos/projetos.component';
 import { DesenvolvedoresComponent } from './desenvolvedores/desenvolvedores.component';
+import { PerfilComponent } from './perfil/perfil.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [	
     AppComponent,
     ProjetosComponent,
-      DesenvolvedoresComponent
+      DesenvolvedoresComponent,
+      PerfilComponent,
+      DashboardComponent
    ],
   imports: [
     BrowserModule,
