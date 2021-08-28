c10ec003f2f8cd3484c54ffe2b78980d41d0ffd0
Vinicius Santos <viniciushacrinha1@gmail.com>
2021-08-28 14:18:54 -0300

    Arrumando a lista de devs e colocando função click

--- src/app/desenvolvedores/desenvolvedores.component.ts
+++ src/app/desenvolvedores/desenvolvedores.component.ts
@@ -6,20 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./desenvolvedores.component.css']
 })
 export class DesenvolvedoresComponent implements OnInit {
- public title = 'Desenvolvedores';
+ 
+  public title = 'Desenvolvedores';
+  public devSelected!: string;
+ 
 
   public dev = [
-    {id:1, name: 'Rodrigo Lobo', position: 'Sócio Diretor'},
-    {id:2, name: 'Marina Cabral', position: 'Analista administrativo'},
-    {id:3, name: 'Thiago Fragoso', position: 'Sócio'},
-    {id:4, name: 'George Soares', position: 'Sócio'},
-    {id:5, name: 'Romulo Martins', position: 'Sócio'},
+    {id:1, name: 'Alessandro Bezerra', position: 'Engenheiro de Dados'},
+    {id:2, name: 'Juliane', position: 'Engenheira de software'},
+    {id:3, name: 'Samuel', position: 'Analista Sênior'},
+    {id:4, name: 'André Silva', position: 'Engenheiro de software'},
+    {id:5, name: 'Anderson Gonçalves', position: 'Estagiário full-stack'},
     {id:6, name: 'Rodrigo Nunes de Lucena', position: 'Desenvolvedor de software sênior'},
     {id:7, name: 'Tomaz Santos Junior', position: 'Engenheiro de sofware full-Stack Sênior'},
     {id:8, name: 'Igor Albuquerque', position: 'Engenheiro de software'},
-    {id:9, name: 'Vinicius Santos', position: 'Estagiário'},
+    {id:9, name: 'Vinicius Santos', position: 'Estagiário Futuro Engenheiro'},
   ];
   constructor() { }
+
+  devSelect(dev: any){
+    this.devSelected = dev.name;
+  }
+  voltar(){
+    this.devSelected = '';
+  }
 
   ngOnInit() {
   }
