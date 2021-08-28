44591a8bb1e965d442cd160709debd6133bca24b
Vinicius Santos <viniciushacrinha1@gmail.com>
2021-08-28 12:07:33 -0300

    Arrumando a lista de projetos, colocando hover e função de click

--- src/app/projetos/projetos.component.ts
+++ src/app/projetos/projetos.component.ts
@@ -8,14 +8,22 @@ import { Component, OnInit } from '@angular/core';
 export class ProjetosComponent implements OnInit {
 
   public title = 'Projetos';
- public projetos = [
-   {id:1, name: "NetWorks", description: "Rede de contatos e negócios"},
-   {id:2, name: "System For T4i", description: "Sistema para administração de serviços"},
-   {id:3, name: "System For Google", description: "Sistema para verificar pré-requisitos de padrões de arquitetura dentro de um software"},
-   {id:4, name: "Upgrade Our System", description: "Melhorias em nosso sistema"},
-   {id:5, name: "Create An Innovative System", description: "Planejar, estruturar e criar um sistema inovador"},
-  ];
+  public selectedProject!: string;
 
+  public projetos = [
+    {id:1, name: "NetWorks", description: "Rede de contatos e negócios"},
+    {id:2, name: "System For T4i", description: "Sistema para administração de serviços"},
+    {id:3, name: "System For Google", description: "Sistema para verificar pré-requisitos de padrões de arquitetura dentro de um software"},
+    {id:4, name: "Upgrade Our System", description: "Melhorias em nosso sistema"},
+    {id:5, name: "Create An Innovative System", description: "Planejar, estruturar e criar um sistema inovador"},
+    ];
+
+    selectProject(project: any){
+      this.selectedProject = project.name;
+    }
+    voltar(){
+      this.selectedProject = '';
+    }
 
   constructor() { }
 
